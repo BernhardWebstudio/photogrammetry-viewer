70ad2b27451bd861383c8837512ce380304efac6
Stephan Tittel <stephan.tittel@tu-darmstadt.de>
2024-12-11 10:23:07 +0100

    multiple output bundles (with and without modelviewer)

--- vite.config.without-modelviewer.ts
+++ vite.config.without-modelviewer.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 import terser from "@rollup/plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    emptyOutDir: false,
     minify: "terser",
     rollupOptions: {
       input: "src/photogrammetry-viewer.ts",
       output: {
         dir: "dist",
-        entryFileNames: "photogrammetry-viewer.js",
+        entryFileNames: "photogrammetry-viewer-without-modelviewer.js",
         format: "es"
       },
       plugins: [terser({ format: { comments: false } })],
@@ -18,5 +20,6 @@ export default defineConfig({
   },
   plugins: [
     dts({ insertTypesEntry: true }),
+    peerDepsExternal()
   ]
 });
